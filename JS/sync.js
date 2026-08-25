68e4a2e0c9651293bd2c16a1a2b2a8a85f1e129c
Mukund-Keshav <mukunddps06@gmail.com>
2026-08-25 09:18:55 +0530

    Added sync.js

---
+++ JS/sync.js
@@ -0,0 +1,7 @@
+console.log("Task One");
+for (let i=1; i<=100000; i++) console.log(i);
+console.log("Task Two");
+for (let i=1; i<=100000; i++) console.log(i);
+console.log("Task Three");
+for (let i=1; i<=100000; i++) console.log(i);
+console.log("Hello World!")
