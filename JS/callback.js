dc4c6a695257c5d7ae358a74c119157d7e4efa9d
Mukund-Keshav <mukunddps06@gmail.com>
2026-08-25 09:47:02 +0530

    Updated callback.js

--- JS/callback.js
+++ JS/callback.js
@@ -2,8 +2,9 @@ function sum(a, b) {
     return a + b;
 }
 
-function sumWithMsg(a, b, sumFunc) {
-    return "The sum of "+a+" & "+b+" is "+sumFunc(a, b)+".";
+function sumWithMsg(a, b, callback) {
+    const result="The sum of "+a+" & "+b+" is "+callback(a, b)+".";
+    return result;
 }
 
 console.log(sumWithMsg(10, 20, sum));
